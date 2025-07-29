6f4779176669f9c1792cc4da350f9ae7e9d38508
ashis02121966 <38763145+ashis02121966@users.noreply.github.com>
2025-07-29 17:34:00 +0530

    Move AuthProvider to main.tsx to fix context error

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-import { AuthProvider, useAuth } from './contexts/AuthContext';
 import { NetworkProvider } from './contexts/NetworkContext';
 import { Login } from './pages/Login';
 import { ProtectedRoute } from './components/ProtectedRoute';
@@ -9,18 +8,16 @@ import { ProtectedRoutes } from './components/ProtectedRoutes';
 function App() {
   return (
     <Router>
-      <AuthProvider>
-        <NetworkProvider>
-          <Routes>
-            <Route path="/login" element={<Login />} />
-            <Route path="/*" element={
-              <ProtectedRoute>
-                <ProtectedRoutes />
-              </ProtectedRoute>
-            } />
-          </Routes>
-        </NetworkProvider>
-      </AuthProvider>
+      <NetworkProvider>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/*" element={
+            <ProtectedRoute>
+              <ProtectedRoutes />
+            </ProtectedRoute>
+          } />
+        </Routes>
+      </NetworkProvider>
     </Router>
   );
 }
